015a6c3639764f7c165e024145f05b1e050b5416
un3580ja0601 <jasmineung123@gmail.com>
2026-02-26 17:54:51 +0000

    LOOP!

--- timezone.py
+++ timezone.py
@@ -22,37 +22,44 @@ def get_city_time(timezone): #procedural abstraction & parameter
 
 all_timezones = get_all_timezones() #function call
 
+while True:
 
-timezone = input("Enter ANY valid time zone (example: America/New_York, Asia/Shanghai, Australia/Sydney, Europe/Rome): ").strip()
- #input
+    timezone = input("Enter ANY valid time zone (example: America/New_York, Asia/Shanghai, Australia/Sydney, Europe/Rome): ").strip()
+    #input
 
-if timezone not in all_timezones: #conditional
-    print("Invalid time zone.")
-else:
-    time_data = get_city_time(timezone) #function call
+    if timezone.lower() == "exit":
+        print("Program ended")
+        break
+
+    if timezone not in all_timezones:
+        print("Invalid time zone.")
+        continue
+    
+    time_data = get_city_time(timezone) 
 
     if time_data is None:
-        print("Error, could not retrieve time.")
+        print("Error, could not get time.")
+        continue
+    
+    format_choice = input("Do you want millitary or regular time format? (m/r): ")
+            
+    hour = time_data["hour"]
+    minute = time_data["minute"]
+
+    if format_choice == "r":
+        suffix = "AM"
+        if hour == 0:
+            hour = 12
+        elif hour == 12:
+            suffix = "PM"
+        elif hour > 12:
+            hour -= 12
+            suffix = "PM"
+
+        print("\nTime Zone:", time_data["timeZone"])
+        print("Current Time:", f"{hour}:{minute:02d} {suffix}")
     else:
-        format_choice = input("Do you want millitary or regular time format? (m/r): ")
+        print("\nTime Zone:", time_data["timeZone"])
+        print("Current Time:", f"{hour}:{minute:02d}")
 
-        hour = time_data["hour"]
-        minute = time_data["minute"]
-
-        if format_choice == "r":
-            suffix = "AM"
-            if hour == 0:
-                hour = 12
-            elif hour == 12:
-                suffix = "PM"
-            elif hour > 12:
-                hour -= 12
-                suffix = "PM"
-
-            print("\nTime Zone:", time_data["timeZone"])
-            print("Current Time:", f"{hour}:{minute:02d} {suffix}")
-        else:
-            print("\nTime Zone:", time_data["timeZone"])
-            print("Current Time:", f"{hour}:{minute:02d}")
-
-        print("Date:", f'{time_data["month"]}/{time_data["day"]}/{time_data["year"]}')
+    print("Date:", f'{time_data["month"]}/{time_data["day"]}/{time_data["year"]}')
